196d1feb111117c6644ebbec0b96ef04e8093765
Ethan Roberts <theLameBrain@gmail.com>
2020-07-13 12:27:32 -0700

    Correction: I am NOW ready to start maze algorithim desing.

--- Assets/Scripts/Classes/MazeTileClass.cs
+++ Assets/Scripts/Classes/MazeTileClass.cs
@@ -3,37 +3,43 @@
 
 public class MazeTileClass
 {
-    private bool west, north, east, south;
+    private bool west, north, east, south, draw;
 
     public MazeTileClass()
     {
-        north = false;
-        west = false; east = false;
+        east = false;
         south = false;
+        draw = false;
     }
 
     public bool IsConnected(string dir)
     {
         bool c = false;
-        if (dir == "west") c = west;
-        if (dir == "north") c = north;
         if (dir == "east") c = east;
         if (dir == "south") c = south;
         return c;
     }
     public void SetConnected(string dir, bool tf)
     {
-        if (dir == "west") west = tf;
-        if (dir == "north") north = tf;
         if (dir == "east") east = tf;
         if (dir == "south") south = tf;
     }
     public void ToggleConnected(string dir)
     {
-        if (dir == "west") west = !west;
-        if (dir == "north") north = !north;
         if (dir == "east") east = !east;
         if (dir == "south") south = !south;
+    }
+    public bool drawn()
+    {
+        return draw;
+    }
+    public void SetTile(bool tf)
+    {
+        draw = tf;
+    }
+    public void ToggleTile()
+    {
+        draw = !draw;
     }
     //866
     //207147977
--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*!!!!!!!!!!!!!!!!!!!!!!!!!SANITY NOTE!!!!!!!!!!!!!!!!!
+ * DoorLR always draw the tile to the Tile's RIGHT.
+ * DoorUD always draw the tile to the Tile's BOTTOM.
+ * that means DoorLR[0,0] and DoorUD[0,0] are valid,
+ * but DoorLR[mazeSize, mazeSize] and DoorUD[mazeSize, mazeSize] are OUT OF RANGE!
+ * Don't go crazy!
+ */
+
 public class Maze : MonoBehaviour
 {
     public GameObject mazeTilePF, maze_UD_DoorPF, maze_LR_DoorPF;
@@ -18,13 +26,14 @@ public class Maze : MonoBehaviour
         if (mazeSize == 0) mazeSize = 9;
         mazeTile = new MazeTileClass[mazeSize, mazeSize];
         tile = new GameObject[mazeSize, mazeSize];
-        doorLR = new GameObject[mazeSize, mazeSize];
-        doorUD = new GameObject[mazeSize, mazeSize];
+        doorLR = new GameObject[mazeSize - 1, mazeSize - 1];
+        doorUD = new GameObject[mazeSize - 1, mazeSize - 1];
         for (int y = 0; y < mazeSize; y++)
         {
             for (int x = 0; x < mazeSize; x++)
             {
                 tile[x, y] = Instantiate(mazeTilePF, new Vector3(x, y, 0), mazeTilePF.transform.rotation);
+                tile[x, y].SetActive(false);                
             }
         } 
         for (int y = 0; y < mazeSize - 1; y++)
@@ -32,14 +41,42 @@ public class Maze : MonoBehaviour
             for (int x = 0; x < mazeSize - 1; x++)
             {
                 doorLR[x, y] = Instantiate(maze_LR_DoorPF, new Vector3(x + 0.5f, y, 0), maze_LR_DoorPF.transform.rotation);
+                doorLR[x, y].SetActive(false);
                 doorUD[x, y] = Instantiate(maze_UD_DoorPF, new Vector3(x, y + 0.5f, 0), maze_UD_DoorPF.transform.rotation);
+                doorUD[x, y].SetActive(false);
             }
         }
+        GenerateMaze();
+        DrawMaze();    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void DrawMaze()
+    {
+        for (int y = 0; y < mazeSize; y++)
+        {
+            for (int x = 0; x < mazeSize; x++)
+            {
+                if(mazeTile[x,y].drawn()) tile[x, y].SetActive(true);
+            }
+        }
+        for (int y = 0; y < mazeSize - 1; y++)
+        {
+            for (int x = 0; x < mazeSize - 1; x++)
+            {
+                if(mazeTile[x,y].IsConnected("east")) doorLR[x, y].SetActive(true);
+                if (mazeTile[x, y].IsConnected("south")) doorUD[x, y].SetActive(true);
+            }
+        }
+    }
+
+    public void GenerateMaze()
+    {
+
     }
 }
