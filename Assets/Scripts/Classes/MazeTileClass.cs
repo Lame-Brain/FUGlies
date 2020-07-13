bf48e65e47796f9d3708d536ba21465b36501aed
Ethan Roberts <theLameBrain@gmail.com>
2020-07-13 11:50:23 -0700

    Initial Commit

---
+++ Assets/Scripts/Classes/MazeTileClass.cs
@@ -0,0 +1,41 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+
+public class MazeTileClass
+{
+    private bool west, north, east, south;
+
+    public MazeTileClass()
+    {
+        north = false;
+        west = false; east = false;
+        south = false;
+    }
+
+    public bool IsConnected(string dir)
+    {
+        bool c = false;
+        if (dir == "west") c = west;
+        if (dir == "north") c = north;
+        if (dir == "east") c = east;
+        if (dir == "south") c = south;
+        return c;
+    }
+    public void SetConnected(string dir, bool tf)
+    {
+        if (dir == "west") west = tf;
+        if (dir == "north") north = tf;
+        if (dir == "east") east = tf;
+        if (dir == "south") south = tf;
+    }
+    public void ToggleConnected(string dir)
+    {
+        if (dir == "west") west = !west;
+        if (dir == "north") north = !north;
+        if (dir == "east") east = !east;
+        if (dir == "south") south = !south;
+    }
+    //866
+    //207147977
+    //503-591-7007-030217-5
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public GameObject mazeTilePF, maze_UD_DoorPF, maze_LR_DoorPF;
+    [HideInInspector]
+    public MazeTileClass[,] mazeTile;
+    [HideInInspector]
+    public GameObject[,] tile, doorLR, doorUD;
+    public int mazeSize;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mazeSize == 0) mazeSize = 9;
+        mazeTile = new MazeTileClass[mazeSize, mazeSize];
+        tile = new GameObject[mazeSize, mazeSize];
+        doorLR = new GameObject[mazeSize, mazeSize];
+        doorUD = new GameObject[mazeSize, mazeSize];
+        for (int y = 0; y < mazeSize; y++)
+        {
+            for (int x = 0; x < mazeSize; x++)
+            {
+                tile[x, y] = Instantiate(mazeTilePF, new Vector3(x, y, 0), mazeTilePF.transform.rotation);
+            }
+        } 
+        for (int y = 0; y < mazeSize - 1;)
+        {
+            for (int x = 0; x < mazeSize - 1; x++)
+            {
+                //doorLR[x, y] = Instantiate(mazeTilePF, new Vector3(x, y, 0), mazeTilePF.transform.rotation);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
